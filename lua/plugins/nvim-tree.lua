25a37bb5d7585f132503cf0221870f3425a3f21c
snake-ch <291222536@qq.com>
2023-04-04 14:04:14 +0800

    avoid nvim-tree warning msg

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -1,7 +1,3 @@
--- disable netrw at the very start of your init.lua (strongly advised)
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
-
 -- OR setup with some options
 require('nvim-tree').setup({
   sort_by = 'case_sensitive',
