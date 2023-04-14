5379d0f059207ab10fc05ac9bdb4bd45a8c150e5
huichen8 <291222536@qq.com>
2023-04-14 09:15:10 +0800

    update null-is sources

--- lua/plugins/null.lua
+++ lua/plugins/null.lua
@@ -2,20 +2,22 @@ local null_ls = require('null-ls')
 
 null_ls.setup({
     sources = {
+        -- go
+        null_ls.builtins.formatting.gofmt,
+        null_ls.builtins.formatting.goimports,
+        null_ls.builtins.formatting.goimports_reviser,
+        null_ls.builtins.diagnostics.golangci_lint,
+        null_ls.builtins.diagnostics.staticcheck,
+        null_ls.builtins.code_actions.impl,
+        null_ls.builtins.code_actions.gomodifytags,
+
         -- python
         null_ls.builtins.formatting.black,
         null_ls.builtins.formatting.isort,
         null_ls.builtins.diagnostics.flake8.with({ extra_args = { '--ignore=E501' } }),
 
-        -- go
-        null_ls.builtins.code_actions.gomodifytags,
-        null_ls.builtins.diagnostics.golangci_lint,
-        null_ls.builtins.diagnostics.staticcheck,
-        null_ls.builtins.formatting.gofmt,
-        null_ls.builtins.formatting.goimports_reviser,
-
         -- protobuf
-        null_ls.builtins.diagnostics.buf,
-        null_ls.builtins.formatting.buf
+        null_ls.builtins.formatting.buf,
+        null_ls.builtins.diagnostics.buf
     },
 })
