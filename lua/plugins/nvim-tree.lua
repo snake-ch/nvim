75a4a3734aff59b8f0d2a45d13740ec6848db4b6
huichen8 <291222536@qq.com>
2023-10-29 03:27:02 +0800

    auto open & close

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -1,11 +1,12 @@
 -- OR setup with some options
 require('nvim-tree').setup({
-  sort_by = 'case_sensitive',
+  -- sort_by = 'case_sensitive',
+  sort_by = 'extension',
   actions = {
     open_file = { quit_on_open = false }
   },
   view = {
-    width = 34,
+    width = 30,
   },
   renderer = {
     group_empty = true
@@ -16,12 +17,33 @@ require('nvim-tree').setup({
   },
   git = {
     enable = false
-  },
-  log = {
-    enable = true,
-    types = {
-      diagnostics = true
-    }
   }
 })
 
+local function open_nvim_tree(data)
+  -- buffer is a real file on the disk
+  local real_file = vim.fn.filereadable(data.file) == 1
+
+  -- buffer is a [No Name]
+  local no_name = data.file == "" and vim.bo[data.buf].buftype == ""
+
+  if not real_file and not no_name then
+    return
+  end
+
+  -- open the tree, find the file but don't focus it
+  require("nvim-tree.api").tree.toggle({ focus = true, find_file = true, })
+end
+
+-- auto open startup
+vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })
+
+-- auto close
+vim.api.nvim_create_autocmd("BufEnter", {
+  nested = true,
+  callback = function()
+    if #vim.api.nvim_list_wins() == 1 and require("nvim-tree.utils").is_nvim_tree_buf() then
+      vim.cmd "quit"
+    end
+  end
+})
