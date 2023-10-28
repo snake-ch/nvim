5950db82274f5709845df49956d93a00d704e4d8
huichen8 <291222536@qq.com>
2023-10-29 03:21:57 +0800

    update pairs & tags cfg

--- lua/plugins/autopairs.lua
+++ lua/plugins/autopairs.lua
@@ -1,12 +1,11 @@
 require('nvim-autopairs').setup({
-  disable_filetype = {},
+  check_ts = true,
+  ts_config = {
+    javascript = { 'template_string' },
+  }
 })
 
 -- If you want insert `(` after select function or method item
 local cmp_autopairs = require('nvim-autopairs.completion.cmp')
 local cmp = require('cmp')
-cmp.event:on(
-  'confirm_done',
-  cmp_autopairs.on_confirm_done()
-)
-
+cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done({ map_char = { tex = '' } }))
