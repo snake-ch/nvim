3688318c9751ed8b22c14886f8472d71da98f307
huichen8 <291222536@qq.com>
2023-10-29 03:24:34 +0800

    remove unused cmds

--- lua/core/autocmd.lua
+++ lua/core/autocmd.lua
@@ -1,47 +1,16 @@
+local autocmd = vim.api.nvim_create_autocmd
 local autoGroup = vim.api.nvim_create_augroup('autoGroup', {
   clear = true,
-})
-
-local autocmd = vim.api.nvim_create_autocmd
-
-autocmd('BufWritePost', {
-  group = autoGroup,
-  -- autocmd BufWritePost plugins-setup.lua source <afile> | PackerSync
-  callback = function()
-    if vim.fn.expand('<afile>') == 'lua/plugins/plugins-setup.lua' then
-      vim.api.nvim_command('source lua/plugins/plugins-setup.lua')
-      vim.api.nvim_command('PackerSync')
-    end
-  end,
 })
 
 -- format on save
 autocmd('BufWritePre', {
   group = autoGroup,
-  pattern = { '*.go', '*.py', '*.lua', '*.proto' },
+  -- pattern = { '*.go', '*.py', '*.lua', '*.proto', '*.css', '*.ts', '*.tsx' },
+  pattern = '*',
   callback = function()
     vim.lsp.buf.format()
   end,
-})
-
--- -- nvim-tree auto close
--- autocmd('BufEnter', {
---   nested = true,
---   group = autoGroup,
---   callback = function()
---     if #vim.api.nvim_list_wins() == 1 and vim.api.nvim_buf_get_name(0):match('NvimTree_') ~= nil then
---       vim.cmd('quit')
---     end
---   end,
--- })
-
--- Highlight on yank
-autocmd('TextYankPost', {
-  callback = function()
-    vim.highlight.on_yank()
-  end,
-  group = autoGroup,
-  pattern = '*',
 })
 
 autocmd('BufEnter', {
