734cf67b511bfb41c642861a1826b4983be08ab0
huichen8 <291222536@qq.com>
2023-10-29 03:25:34 +0800

    update formatting

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -1,21 +1,67 @@
-local cmp_status_ok, cmp = pcall(require, 'cmp')
-if not cmp_status_ok then
-  return
-end
-
-local kind_status_ok, lspkind = pcall(require, 'lspkind')
-if not kind_status_ok then
-  return
-end
+local cmp = require('cmp')
+local feedkeys = require('cmp.utils.feedkeys')
+local types = require("cmp.types")
 
 local has_words_before = function()
   unpack = unpack or table.unpack
   local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match('%s') == nil
+  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
 end
 
-local feedkey = function(key, mode)
-  vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(key, true, true, true), mode, true)
+local kind_icons = {
+  Text = "",
+  Method = "m",
+  Function = "",
+  Constructor = "",
+  Field = "",
+  Variable = "",
+  Class = "",
+  Interface = "",
+  Module = "",
+  Property = "",
+  Unit = "",
+  Value = "",
+  Enum = "",
+  Keyword = "",
+  Snippet = "",
+  Color = "",
+  File = "",
+  Reference = "",
+  Folder = "",
+  EnumMember = "",
+  Constant = "",
+  Struct = "",
+  Event = "",
+  Operator = "",
+  TypeParameter = "",
+}
+
+local function limit_lsp_types(entry, ctx)
+  local kind = entry:get_kind()
+  local line = ctx.cursor.line
+  local col = ctx.cursor.col
+  local char_before_cursor = string.sub(line, col - 1, col - 1)
+  local char_after_dot = string.sub(line, col, col)
+
+  if char_before_cursor == "." and char_after_dot:match("[a-zA-Z]") then
+    if
+        kind == types.lsp.CompletionItemKind.Method
+        or kind == types.lsp.CompletionItemKind.Field
+        or kind == types.lsp.CompletionItemKind.Property
+    then
+      return true
+    else
+      return false
+    end
+  elseif string.match(line, "^%s+%w+$") then
+    if kind == types.lsp.CompletionItemKind.Function or kind == types.lsp.CompletionItemKind.Variable then
+      return true
+    else
+      return false
+    end
+  end
+
+  return true
 end
 
 cmp.setup({
@@ -25,8 +71,11 @@ cmp.setup({
       vim.fn['vsnip#anonymous'](args.body) -- For `vsnip` users.
     end
   },
+  window = {
+    completion = cmp.config.window.bordered(),
+    documentation = cmp.config.window.bordered(),
+  },
   mapping = cmp.mapping.preset.insert({
-    -- Use <C-b/f> to scroll the docs
     ['<C-b>'] = cmp.mapping.scroll_docs(-4),
     ['<C-f>'] = cmp.mapping.scroll_docs(4),
     ['<C-Space>'] = cmp.mapping.complete(),
@@ -38,64 +87,44 @@ cmp.setup({
       if cmp.visible() then
         cmp.select_next_item()
       elseif vim.fn['vsnip#available'](1) == 1 then
-        feedkey('<Plug>(vsnip-expand-or-jump)', '')
+        feedkeys.call('<Plug>(vsnip-expand-or-jump)', '')
       elseif has_words_before() then
         cmp.complete()
       else
         fallback() -- The fallback function sends a already mapped key. In this case, it's probably `<Tab>`.
       end
     end, { 'i', 's' }),
+
     ['<S-Tab>'] = cmp.mapping(function()
       if cmp.visible() then
         cmp.select_prev_item()
       elseif vim.fn['vsnip#jumpable'](-1) == 1 then
-        feedkey('<Plug>(vsnip-jump-prev)', '')
+        feedkeys.call('<Plug>(vsnip-jump-prev)', '')
       end
     end, { 'i', 's' }),
     -- end of super Tab
   }),
   formatting = {
-    format = lspkind.cmp_format({
-      mode = 'symbol_text',  -- show only symbol annotations
-      maxwidth = 50,         -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
-      ellipsis_char = '...', -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
-      -- The function below will be called before any actual modifications from lspkind
-      -- so that you can provide more controls on popup customization. (See [#30](https://github.com/onsails/lspkind-nvim/pull/30))
-      before = function(entry, vim_item)
-        -- if vim.tbl_contains({ 'path' }, entry.source.name) then
-        --   local icon, hl_group = require('nvim-web-devicons').get_icon(entry:get_completion_item().label)
-        --   if icon then
-        --     vim_item.kind = icon
-        --     vim_item.kind_hl_group = hl_group
-        --     return vim_item
-        --   end
-        -- end
-        vim_item.menu = ({
-          nvim_lsp = '[Lsp]',
-          vsnip = '[Vsnip]',
-          buffer = '[File]',
-          path = '[Path]',
-        })[entry.source.name]
-        return vim_item
-      end
-    })
+    fields = { "kind", "abbr", "menu" },
+    format = function(entry, vim_item)
+      -- Kind icons
+      vim_item.kind = string.format("%s", kind_icons[vim_item.kind])
+      -- vim_item.kind = string.format('%s %s', kind_icons[vim_item.kind], vim_item.kind) -- This concatonates the icons with the name of the item kind
+      -- vim_item.menu = ({
+      --   nvim_lsp = "[LSP]",
+      --   luasnip = "[Snippet]",
+      --   buffer = "[Buffer]",
+      --   path = "[Path]",
+      -- })[entry.source.name]
+      return vim_item
+    end,
   },
   sources = cmp.config.sources({
-    { name = 'nvim_lsp' },
-    { name = 'vsnip' }, -- For vsnip users.
-  }, {
-    { name = 'buffer' },
-    { name = 'path' }
+    { name = 'path' },
+    -- { name = 'buffer' },
+    { name = 'nvim_lsp', entry_filter = limit_lsp_types, },
+    { name = 'vsnip', },
   }),
-})
-
--- Set configuration for specific filetype.
-cmp.setup.filetype('gitcommit', {
-  sources = cmp.config.sources({
-    { name = 'cmp_git' } -- You can specify the `cmp_git` source if you were installed it.
-  }, {
-    { name = 'buffer' }
-  })
 })
 
 -- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
@@ -115,4 +144,3 @@ cmp.setup.cmdline(':', {
     { name = 'cmdline' }
   })
 })
-
