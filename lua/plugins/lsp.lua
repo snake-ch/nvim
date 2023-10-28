d91f18250f2f2b55b0fea10c4be71971d93a635f
huichen8 <291222536@qq.com>
2023-10-29 03:22:16 +0800

    more lsp servers

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,10 +1,20 @@
 local capabilities = require('cmp_nvim_lsp').default_capabilities()
+local lspconfig = require('lspconfig')
 
-local servers = { 'gopls', 'pyright', 'lua_ls' }
+-- Enable some language servers with the additional completion capabilities offered by nvim-cmp
+local servers = {
+  'gopls',
+  'lua_ls',
+  'emmet_language_server',
+  'tsserver',
+  'cssls',
+  'tailwindcss',
+}
 -- Use a loop to conveniently call 'setup' on multiple servers and
 -- map buffer local keybindings when the language server attaches
-for _, lsp in pairs(servers) do
-  require('lspconfig')[lsp].setup {
-    capabilities = capabilities
+for _, lsp in ipairs(servers) do
+  lspconfig[lsp].setup {
+    -- on_attach = my_custom_on_attach,
+    capabilities = capabilities,
   }
 end
