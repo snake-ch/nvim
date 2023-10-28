c28bb3f5f1b40fc902dbb3a0633156e2a6b6207f
huichen8 <291222536@qq.com>
2023-10-29 03:28:23 +0800

    reduce plugins

--- lua/plugins/null.lua
+++ lua/plugins/null.lua
@@ -3,23 +3,28 @@ local null_ls = require('null-ls')
 null_ls.setup({
   sources = {
     -- go
-    null_ls.builtins.formatting.gofmt,
+    -- null_ls.builtins.formatting.gofmt,
     -- null_ls.builtins.formatting.goimports,
-    null_ls.builtins.formatting.goimports_reviser,
-    null_ls.builtins.diagnostics.golangci_lint,
-    null_ls.builtins.diagnostics.staticcheck,
+    -- null_ls.builtins.formatting.goimports_reviser,
+    -- null_ls.builtins.diagnostics.golangci_lint,
+    -- null_ls.builtins.diagnostics.staticcheck,
     null_ls.builtins.code_actions.impl,
     null_ls.builtins.code_actions.gomodifytags,
 
     -- python
-    null_ls.builtins.formatting.black,
-    null_ls.builtins.formatting.isort,
-    null_ls.builtins.diagnostics.flake8.with({ extra_args = { '--ignore=E501' } }),
+    -- null_ls.builtins.formatting.black,
+    -- null_ls.builtins.formatting.isort,
+    -- null_ls.builtins.diagnostics.flake8.with({ extra_args = { '--ignore=E501' } }),
 
     -- protobuf
     null_ls.builtins.formatting.buf,
     null_ls.builtins.diagnostics.buf,
     -- null_ls.builtins.formatting.protolint,
     -- null_ls.builtins.diagnostics.protolint,
+
+    -- typescript
+    -- null_ls.builtins.formatting.prettier,
+    -- null_ls.builtins.diagnostics.eslint,
+    -- null_ls.builtins.code_actions.eslint,
   },
 })
