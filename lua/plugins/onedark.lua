b071c58f80872be05a1d4362f59a9dfd882823c3
huichen8 <291222536@qq.com>
2024-05-19 20:07:24 +0800

    lua format

--- lua/plugins/onedark.lua
+++ lua/plugins/onedark.lua
@@ -8,7 +8,7 @@ require('onedark').setup {
   cmp_itemkind_reverse = false, -- reverse item kind highlights in cmp menu
 
   -- toggle theme style ---
-  toggle_style_key = nil,                                                            -- keybind to toggle theme style. Leave it nil to disable it, or set it to a string, for example '<leader>ts'
+  toggle_style_key = nil,                                                              -- keybind to toggle theme style. Leave it nil to disable it, or set it to a string, for example '<leader>ts'
   toggle_style_list = { 'dark', 'darker', 'cool', 'deep', 'warm', 'warmer', 'light' }, -- List of styles to toggle between
 
   -- Change code style ---
@@ -24,7 +24,7 @@ require('onedark').setup {
 
   -- Lualine options --
   lualine = {
-    transparent = true,   -- lualine center bar transparency
+    transparent = true, -- lualine center bar transparency
   },
 
   -- Custom Highlights --
@@ -33,10 +33,9 @@ require('onedark').setup {
 
   -- Plugins Config --
   diagnostics = {
-    darker = true,       -- darker colors for diagnostic
-    undercurl = true,    -- use undercurl instead of underline for diagnostics
-    background = true,   -- use background color for virtual text
+    darker = true,     -- darker colors for diagnostic
+    undercurl = true,  -- use undercurl instead of underline for diagnostics
+    background = true, -- use background color for virtual text
   },
 }
 require('onedark').load()
-
