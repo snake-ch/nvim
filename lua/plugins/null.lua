329c3f84f962de15e6651a90f92196397b4a1baa
huichen8 <291222536@qq.com>
2024-06-15 20:59:42 +0800

    update configs from repo recommand

--- lua/plugins/null.lua
+++ lua/plugins/null.lua
@@ -1,13 +1,11 @@
 local null_ls = require('null-ls')
-local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+local augroup = vim.api.nvim_create_augroup('LspFormatting', {})
 
 local sources = {
   -- go
-  -- null_ls.builtins.formatting.gofmt,
   null_ls.builtins.formatting.goimports,
   null_ls.builtins.formatting.goimports_reviser,
   null_ls.builtins.diagnostics.golangci_lint,
-  -- null_ls.builtins.diagnostics.staticcheck,
   null_ls.builtins.code_actions.impl,
   null_ls.builtins.code_actions.gomodifytags,
 
@@ -21,32 +19,8 @@ local sources = {
   null_ls.builtins.diagnostics.buf,
 
   -- typescript
-  null_ls.builtins.formatting.prettier,
+  -- null_ls.builtins.formatting.prettier,
   null_ls.builtins.formatting.rustywind,
 }
 
-null_ls.setup({
-  sources = sources,
-
-  -- you can reuse a shared lspconfig on_attach callback here
-  -- on_attach = function(client, bufnr)
-  --   if client.supports_method("textDocument/formatting") then
-  --     vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-  --     vim.api.nvim_create_autocmd("BufWritePre", {
-  --       group = augroup,
-  --       buffer = bufnr,
-  --       callback = function()
-  --         vim.lsp.buf.format({
-  --           async = false,
-  --           timeout_ms = 5000,
-  --           -- filter = function(client)
-  --           --   -- apply whatever logic you want (in this example, we'll only use null-ls)
-  --           --   return client.name == "null-ls"
-  --           -- end,
-  --           bufnr = bufnr,
-  --         })
-  --       end,
-  --     })
-  --   end
-  -- end,
-})
+null_ls.setup({ sources = sources })
