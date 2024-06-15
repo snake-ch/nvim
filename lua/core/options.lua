895ac05a51d7af78c0a496d93801eb3869cd0e04
huichen8 <291222536@qq.com>
2024-06-15 20:56:54 +0800

    updated

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -1,49 +1,38 @@
-local opt = vim.opt
+vim.g.mapleader = ';'
 
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
+-- Hint: use `:h <option>` to figure out the meaning if needed
+vim.opt.clipboard = 'unnamedplus'                       -- use system clipboard
+vim.opt.completeopt = { 'menu', 'menuone', 'noselect' } --
+vim.opt.mouse = 'a'                                     -- allow the mouse to be used in Nvim
 
-opt.number = true
-opt.relativenumber = true
+-- Tab
+vim.opt.tabstop = 2      -- number of visual spaces per TAB
+vim.opt.softtabstop = 2  -- number of spacesin tab when editing
+vim.opt.shiftwidth = 2   -- insert 4 spaces on a tab
+vim.opt.expandtab = true -- tabs are spaces
 
-opt.encoding = 'utf-8'
-opt.fileencoding = 'utf-8'
-vim.scriptencoding = 'utf-8'
+-- UI config
+vim.opt.guicursor = 'n-v-c:block,i-ci-ve:ver25,r-cr:hor20,o:hor50,a:blinkwait700-blinkoff400-blinkon250'
+vim.opt.termguicolors = true  -- enabl 24-bit RGB color in the TUI
+vim.opt.number = true         -- show absolute number
+vim.opt.relativenumber = true -- add numbers to each line on the left side
+vim.opt.cursorline = true     -- highlight cursor line underneath the cursor horizontally
+vim.opt.splitbelow = true     -- open new vertical split bottom
+vim.opt.splitright = true     -- open new horizontal splits right
+vim.opt.cmdheight = 0         -- cmd height
 
-opt.scrolloff = 5
-opt.sidescrolloff = 5
+-- Edit
+vim.opt.wrap = false                 -- disable line wrap
+vim.opt.scrolloff = 5                -- reserve rows
+vim.opt.sidescrolloff = 5            -- reserve columns
+vim.opt.signcolumn = 'yes'           -- always show the signcolumn, otherwise it would shift the text each time
+vim.opt.list = false                 -- show some invisible characters (tabs...
+vim.opt.listchars = 'space:·,tab:>-' -- chars for space & tab
+vim.opt.foldlevel = 99               -- numbers of fold
+vim.opt.foldmethod = 'indent'        -- fold method
 
-opt.hlsearch = true
-opt.incsearch = true
-
-opt.guicursor = 'n-v-c-sm:block,i-ci-ve:ver25,r-cr-o:hor20,a:blinkwait700-blinkoff400-blinkon250'
-opt.mouse = 'a'
-opt.clipboard:append('unnamedplus')
-
-opt.tabstop = 2
-opt.softtabstop = 2
-opt.shiftwidth = 2
-opt.expandtab = true
-
-opt.ignorecase = true
-opt.smartcase = true
-
-opt.wrap = false
-opt.swapfile = false
-opt.autoread = true
-vim.bo.autoread = true
-
-opt.signcolumn = 'yes'
--- opt.list = true
-vim.listchars = 'space:·'
-
-opt.cursorline = true
-opt.termguicolors = true
-
-opt.updatetime = 300
-opt.timeoutlen = 500
-
-opt.cmdheight = 0
-
-vim.wo.foldmethod = 'indent'
-vim.wo.foldlevel = 99
+-- Searching
+vim.opt.incsearch = true  -- search as characters are entered
+vim.opt.hlsearch = true   -- highlight matches
+vim.opt.ignorecase = true -- ignore case in searches by default
+vim.opt.smartcase = true  -- but make it case sensitive if an uppercase is entered
