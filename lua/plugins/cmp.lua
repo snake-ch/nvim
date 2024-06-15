1ee66cebff71ceacde66d4546d060d8b24cde935
huichen8 <291222536@qq.com>
2024-06-15 21:00:05 +0800

    change to luasnip

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -1,74 +1,45 @@
+local luasnip = require('luasnip')
 local cmp = require('cmp')
-local feedkeys = require('cmp.utils.feedkeys')
-local types = require("cmp.types")
+require('luasnip.loaders.from_vscode').lazy_load()
+
+local kind_icons = {
+  Text = '󰉿',
+  Method = '󰆧',
+  Function = '󰊕',
+  Constructor = '',
+  Field = '󰜢',
+  Variable = '󰀫',
+  Class = '󰠱',
+  Interface = '',
+  Module = '',
+  Property = '󰜢',
+  Unit = '󰑭',
+  Value = '󰎠',
+  Enum = '',
+  Keyword = '󰌋',
+  Snippet = '',
+  Color = '󰏘',
+  File = '󰈙',
+  Reference = '󰈇',
+  Folder = '󰉋',
+  EnumMember = '',
+  Constant = '󰏿',
+  Struct = '󰙅',
+  Event = '',
+  Operator = '󰆕',
+  TypeParameter = '',
+}
 
 local has_words_before = function()
   unpack = unpack or table.unpack
   local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-end
-
-local kind_icons = {
-  Text = "",
-  Method = "m",
-  Function = "",
-  Constructor = "",
-  Field = "",
-  Variable = "",
-  Class = "",
-  Interface = "",
-  Module = "",
-  Property = "",
-  Unit = "",
-  Value = "",
-  Enum = "",
-  Keyword = "",
-  Snippet = "",
-  Color = "",
-  File = "",
-  Reference = "",
-  Folder = "",
-  EnumMember = "",
-  Constant = "",
-  Struct = "",
-  Event = "",
-  Operator = "",
-  TypeParameter = "",
-}
-
-local function limit_lsp_types(entry, ctx)
-  local kind = entry:get_kind()
-  local line = ctx.cursor.line
-  local col = ctx.cursor.col
-  local char_before_cursor = string.sub(line, col - 1, col - 1)
-  local char_after_dot = string.sub(line, col, col)
-
-  if char_before_cursor == "." and char_after_dot:match("[a-zA-Z]") then
-    if
-        kind == types.lsp.CompletionItemKind.Method
-        or kind == types.lsp.CompletionItemKind.Field
-        or kind == types.lsp.CompletionItemKind.Property
-    then
-      return true
-    else
-      return false
-    end
-  elseif string.match(line, "^%s+%w+$") then
-    if kind == types.lsp.CompletionItemKind.Function or kind == types.lsp.CompletionItemKind.Variable then
-      return true
-    else
-      return false
-    end
-  end
-
-  return true
+  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match('%s') == nil
 end
 
 cmp.setup({
   snippet = {
-    -- REQUIRED - you must specify a snippet engine
     expand = function(args)
-      vim.fn['vsnip#anonymous'](args.body) -- For `vsnip` users.
+      luasnip.lsp_expand(args.body) -- For `luasnip` users.
     end
   },
   window = {
@@ -80,14 +51,24 @@ cmp.setup({
     ['<C-f>'] = cmp.mapping.scroll_docs(4),
     ['<C-Space>'] = cmp.mapping.complete(),
     ['<C-e>'] = cmp.mapping.abort(),
-    ['<CR>'] = cmp.mapping.confirm({ select = false }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+    ['<CR>'] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        if luasnip.expandable() then
+          luasnip.expand()
+        else
+          cmp.confirm({ select = true })
+        end
+      else
+        fallback()
+      end
+    end),
     -- super Tab
-    -- sourc: https://github.com/hrsh7th/nvim-cmp/wiki/Example-mappings#vim-vsnip
+    -- sourc: https://github.com/hrsh7th/nvim-cmp/wiki/Example-mappings
     ['<Tab>'] = cmp.mapping(function(fallback)
       if cmp.visible() then
         cmp.select_next_item()
-      elseif vim.fn['vsnip#available'](1) == 1 then
-        feedkeys.call('<Plug>(vsnip-expand-or-jump)', '')
+      elseif luasnip.locally_jumpable(1) then
+        luasnip.jump(1)
       elseif has_words_before() then
         cmp.complete()
       else
@@ -95,35 +76,37 @@ cmp.setup({
       end
     end, { 'i', 's' }),
 
-    ['<S-Tab>'] = cmp.mapping(function()
+    ['<S-Tab>'] = cmp.mapping(function(fallback)
       if cmp.visible() then
         cmp.select_prev_item()
-      elseif vim.fn['vsnip#jumpable'](-1) == 1 then
-        feedkeys.call('<Plug>(vsnip-jump-prev)', '')
+      elseif luasnip.jumpable(-1) then
+        luasnip.jump(-1)
+      else
+        fallback()
       end
     end, { 'i', 's' }),
     -- end of super Tab
   }),
   formatting = {
-    fields = { "kind", "abbr", "menu" },
+    fields = { 'kind', 'abbr', 'menu' },
     format = function(entry, vim_item)
       -- Kind icons
-      vim_item.kind = string.format("%s", kind_icons[vim_item.kind])
-      -- vim_item.kind = string.format('%s %s', kind_icons[vim_item.kind], vim_item.kind) -- This concatonates the icons with the name of the item kind
-      -- vim_item.menu = ({
-      --   nvim_lsp = "[LSP]",
-      --   luasnip = "[Snippet]",
-      --   buffer = "[Buffer]",
-      --   path = "[Path]",
-      -- })[entry.source.name]
+      vim_item.kind = (kind_icons[vim_item.kind] or '')
+      -- Source
+      vim_item.menu = ({
+        nvim_lsp = '[LSP]',
+        luasnip = '[LuaSnip]',
+        buffer = '[Buffer]',
+        path = '[Path]',
+      })[entry.source.name]
       return vim_item
     end,
   },
   sources = cmp.config.sources({
+    { name = 'nvim_lsp' },
+    { name = 'luasnip' }, -- For luasnip users.
     { name = 'path' },
     -- { name = 'buffer' },
-    { name = 'nvim_lsp', entry_filter = limit_lsp_types, },
-    { name = 'vsnip', },
   }),
 })
 
@@ -142,5 +125,6 @@ cmp.setup.cmdline(':', {
     { name = 'path' }
   }, {
     { name = 'cmdline' }
-  })
+  }),
+  matching = { disallow_symbol_nonprefix_matching = false }
 })
