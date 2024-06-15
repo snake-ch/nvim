217d40a20198eae31989b4c38f70d810d27dd19e
huichen8 <291222536@qq.com>
2024-06-15 20:56:45 +0800

    remove autosave

--- lua/core/autocmd.lua
+++ lua/core/autocmd.lua
@@ -1,16 +1,5 @@
 local autocmd = vim.api.nvim_create_autocmd
-local autoGroup = vim.api.nvim_create_augroup('autoGroup', {
-  clear = true,
-})
-
--- format on save
-autocmd('BufWritePre', {
-  group = autoGroup,
-  pattern = { '*.*' },
-  callback = function(args)
-    vim.lsp.buf.format({ async = false, timeout_ms = 5000, bufnr = args.buf })
-  end,
-})
+local autoGroup = vim.api.nvim_create_augroup('autoGroup', { clear = true })
 
 autocmd('BufEnter', {
   group = autoGroup,
