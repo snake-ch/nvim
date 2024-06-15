7b5465a19d8bfc6bd0843f0a1b29e62ae79baf7c
huichen8 <291222536@qq.com>
2024-06-15 21:00:31 +0800

    remove unused configs

--- lua/plugins/lspsaga.lua
+++
@@ -1,9 +0,0 @@
-require('lspsaga').setup({
-  ui = {
-    border = 'rounded',
-  },
-  symbol_in_winbar = { enable = false },
-  lightbulb = { enable = false },
-  beacon = { enable = false },
-  outline = { win_width = 34 },
-})
