052bb2d7e877e88876d9850a7609b36275584a0a
huichen8 <291222536@qq.com>
2024-06-21 21:12:23 +0800

    copy/paste from remote

--- lua/core/autocmd.lua
+++ lua/core/autocmd.lua
@@ -10,3 +10,36 @@ autocmd('BufEnter', {
         + 'r' -- But do continue when pressing enter.
   end,
 })
+
+-- Copy wsl via windows clipboard
+if vim.fn.has('wsl') then
+  autocmd({ 'TextYankPost' }, {
+    group = autoGroup,
+    pattern = '*',
+    callback = function()
+      vim.cmd("call system('/mnt/c/windows/system32/clip.exe ',@\")")
+    end,
+  })
+end
+
+-- Copy/Paste when using ssh on a remote servers
+if vim.clipboard and vim.clipboard.osc52 then
+  autocmd('VimEnter', {
+    group = autoGroup,
+    callback = function()
+      if vim.env.SSH_CONNECTION and vim.clipboard.osc52 then
+        vim.g.clipboard = {
+          name = 'OSC 52',
+          copy = {
+            ['+'] = require('vim.clipboard.osc52').copy,
+            ['*'] = require('vim.clipboard.osc52').copy,
+          },
+          paste = {
+            ['+'] = require('vim.clipboard.osc52').paste,
+            ['*'] = require('vim.clipboard.osc52').paste,
+          },
+        }
+      end
+    end,
+  })
+end
