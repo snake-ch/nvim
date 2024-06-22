3c72a5f3b45992ae18d9db932217cb828ffb728c
huichen8 <291222536@qq.com>
2024-06-22 13:59:59 +0800

    format code

--- lua/config/plugins.lua
+++ lua/config/plugins.lua
@@ -100,15 +100,15 @@ return {
     end,
     dependencies = {
       -- source
-      'hrsh7th/cmp-nvim-lsp',
-      'hrsh7th/cmp-buffer',
-      'hrsh7th/cmp-path',
-      'hrsh7th/cmp-cmdline',
+      { 'hrsh7th/cmp-nvim-lsp' },
+      { 'hrsh7th/cmp-buffer' },
+      { 'hrsh7th/cmp-path' },
+      { 'hrsh7th/cmp-cmdline' },
       -- luasnip
       'L3MON4D3/LuaSnip',
       'saadparwaiz1/cmp_luasnip',
       -- snippets
-      'rafamadriz/friendly-snippets'
+      { 'rafamadriz/friendly-snippets' },
     }
   },
   {
