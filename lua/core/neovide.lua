aabea2d5f6bc41228bdfe0af154b5893e5a297d1
huichen8 <291222536@qq.com>
2024-07-05 10:13:17 +0800

    add neovide config

---
+++ lua/core/neovide.lua
@@ -0,0 +1,6 @@
+if vim.g.neovide then
+  -- Put anything you want to happen only in Neovide here
+  vim.o.guifont = "CaskaydiaCove Nerd Font Mono:h14" -- text below applies for VimScript
+  vim.g.neovide_transparency = 0.8
+  vim.g.neovide_remember_window_size = true
+end
