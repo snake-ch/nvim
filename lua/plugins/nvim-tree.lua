07961b92b4000e1702bf639c613ee41ee8da5c16
huichen8 <291222536@qq.com>
2025-01-11 02:52:58 +0800

    removes commented code

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -20,24 +20,6 @@ require('nvim-tree').setup({
   }
 })
 
--- open on startup
-local function open_nvim_tree(data)
-  -- buffer is a real file on the disk
-  local real_file = vim.fn.filereadable(data.file) == 1
-
-  -- buffer is a [No Name]
-  local no_name = data.file == '' and vim.bo[data.buf].buftype == ''
-
-  if not real_file and not no_name then
-    return
-  end
-
-  -- open the tree, find the file but don't focus it
-  require('nvim-tree.api').tree.toggle({ focus = false, find_file = true, })
-end
-
--- vim.api.nvim_create_autocmd({ 'VimEnter' }, { callback = open_nvim_tree })
-
 -- auto close
 local function tab_win_closed(winnr)
   local api = require 'nvim-tree.api'
