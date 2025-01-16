276c1b3d48f90b47d5cc13d97992200e1c626997
huichen8 <291222536@qq.com>
2025-01-16 10:21:29 +0800

    change import path

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -15,6 +15,7 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require('lazy').setup('config.plugins', {
+require('lazy').setup({
   ui = { border = 'rounded' },
+  spec = { { import = 'plugins' } }
 })
