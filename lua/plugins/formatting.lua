6abfb7ddeb561c9d77023a22b050312f04f6b425
huichen8 <291222536@qq.com>
2025-02-06 16:55:06 +0800

    add formatter

---
+++ lua/plugins/formatting.lua
@@ -0,0 +1,25 @@
+return {
+  {
+    'stevearc/conform.nvim',
+    opts = {
+      format_on_save = {
+        timeout_ms = 5000,
+        lsp_format = 'fallback'
+      },
+      formatters_by_ft = {
+        go = { 'goimports', 'goimports-reviser' },
+        proto = { 'buf' },
+        html = { 'prettierd' },
+        css = { 'prettierd' },
+        json = { 'prettierd' },
+        yaml = { 'prettierd' },
+        markdown = { 'prettierd' },
+        javascript = { 'prettierd', stop_after_first = true },
+        typescript = { 'prettierd', stop_after_first = true },
+        javascriptreact = { 'prettierd', stop_after_first = true },
+        typescriptreact = { 'prettierd', stop_after_first = true },
+        ['_'] = { 'trim_whitespace' }
+      }
+    }
+  }
+}
