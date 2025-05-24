a723be8a7ff9c23279fc999d8910387739ae55f9
huichen8 <291222536@qq.com>
2025-05-24 16:01:35 +0800

    make them silent

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,6 +1,6 @@
 -- Buffer
-vim.keymap.set('n', '<C-L>', ':bnext<CR>', { desc = 'Next Buffer' })
-vim.keymap.set('n', '<C-H>', ':bprevious<CR>', { desc = 'Prev Buffer' })
-vim.keymap.set('n', '<leader>bd', ':bdelete<CR>', { desc = 'Delete Buffer' })
+vim.keymap.set('n', '<C-L>', ':bnext<CR>', { silent = true, desc = 'Next Buffer' })
+vim.keymap.set('n', '<C-H>', ':bprevious<CR>', { silent = true, desc = 'Prev Buffer' })
+vim.keymap.set('n', '<leader>bd', ':bdelete<CR>', { silent = true, desc = 'Delete Buffer' })
 -- Clear search
 vim.keymap.set('n', '<ESC>', vim.cmd.nohlsearch, { desc = 'Clear Highlights' })
