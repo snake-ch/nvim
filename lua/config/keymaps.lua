ecb8a640b96bd4826c9e1e214ec11703e1375c5a
snake-ch <291222536@qq.com>
2025-11-12 09:47:53 +0800

    add slient option

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -3,4 +3,4 @@ vim.keymap.set('n', '<C-L>', ':bnext<CR>', { silent = true, desc = 'Next Buffer'
 vim.keymap.set('n', '<C-H>', ':bprevious<CR>', { silent = true, desc = 'Prev Buffer' })
 vim.keymap.set('n', '<leader>bd', ':bdelete<CR>', { silent = true, desc = 'Delete Buffer' })
 -- Clear search
-vim.keymap.set('n', '<ESC>', vim.cmd.nohlsearch, { desc = 'Clear Highlights' })
+vim.keymap.set('n', '<ESC>', vim.cmd.nohlsearch, { silent = true, desc = 'Clear Highlights' })
