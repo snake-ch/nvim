599be88e5e2404c1166f8eb2810cf2d571370fe0
snake-ch <291222536@qq.com>
2026-04-15 10:49:16 +0800

    remove nvim-treesitter in neovim0.12+

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,56 +1,10 @@
 return {
-  -- Treesitter is a new parser generator tool that we can
-  -- use in Neovim to power faster and more accurate
-  -- syntax highlighting.
-  {
-    'nvim-treesitter/nvim-treesitter',
-    branch = 'master',
-    lazy = false,
-    build = ':TSUpdate',
-    dependencies = { 'nvim-treesitter/nvim-treesitter-textobjects' },
-    opts = {
-      -- a list of parser names, or 'all' (the five listed parsers should always be installed)
-      ensure_installed = { 'lua' },
-
-      -- automatically install missing parsers when entering buffer
-      -- recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-      auto_install = true,
-
-      highlight = { enable = true },
-
-      textobjects = {
-        select = {
-          enable = true,
-
-          -- automatically jump forward to textobj, similar to targets.vim
-          lookahead = true,
-
-          keymaps = {
-            ['af'] = '@function.outer',
-            ['if'] = '@function.inner',
-            ['ac'] = '@class.outer',
-            ['ic'] = '@class.inner',
-            ['al'] = '@loop.outer',
-            ['il'] = '@loop.inner'
-          },
-          include_surrounding_whitespace = true
-        },
-        swap = {
-          enable = true,
-          swap_next = { ['<leader>a'] = '@parameter.inner' },
-          swap_previous = { ['<leader>A'] = '@parameter.inner' }
-        },
-        move = {
-          enable = true,
-          goto_next_start = { [']f'] = '@function.outer', [']c'] = '@class.outer', [']a'] = '@parameter.inner' },
-          goto_next_end = { [']F'] = '@function.outer', [']C'] = '@class.outer', [']A'] = '@parameter.inner' },
-          goto_previous_start = { ['[f'] = '@function.outer', ['[c'] = '@class.outer', ['[a'] = '@parameter.inner' },
-          goto_previous_end = { ['[F'] = '@function.outer', ['[C'] = '@class.outer', ['[A'] = '@parameter.inner' }
-        }
-      }
-    },
-    config = function(_, opts)
-      require('nvim-treesitter.configs').setup(opts)
-    end
-  }
+  'romus204/tree-sitter-manager.nvim',
+  dependencies = {}, -- tree-sitter CLI must be installed system-wide
+  config = function()
+    require('tree-sitter-manager').setup({
+      border = 'rounded',
+      auto_install = true
+    })
+  end
 }
