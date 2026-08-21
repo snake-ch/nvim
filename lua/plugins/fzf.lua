53df82bec899e77a4f2fc555f8c0cb664446e3dc
snake-ch <291222536@qq.com>
2026-08-21 14:43:35 +0800

    chore: replace neogit with fzf-lua git_diff on <leader>gg

--- lua/plugins/fzf.lua
+++ lua/plugins/fzf.lua
@@ -3,10 +3,12 @@ return {
     'ibhagwan/fzf-lua',
     dependencies = { 'nvim-tree/nvim-web-devicons' },
     keys = {
-      { '<leader>ff', '<cmd>FzfLua files<cr>',     desc = 'FzfLuz find files', remap = true },
-      { '<leader>fg', '<cmd>FzfLua live_grep<cr>', desc = 'FzfLuz live grep',  remap = true },
-      { '<leader>fb', '<cmd>FzfLua buffers<cr>',   desc = 'FzfLuz buffers',    remap = true },
-      { '<leader>fh', '<cmd>FzfLua helptags<cr>',  desc = 'FzfLuz help tags',  remap = true }
+      { '<leader>ff', '<cmd>FzfLua files<cr>',     desc = 'FzfLua find files', remap = true },
+      { '<leader>fg', '<cmd>FzfLua live_grep<cr>', desc = 'FzfLua live grep',  remap = true },
+      { '<leader>fb', '<cmd>FzfLua buffers<cr>',   desc = 'FzfLua buffers',    remap = true },
+      { '<leader>fh', '<cmd>FzfLua helptags<cr>',  desc = 'FzfLua help tags',  remap = true },
+      { '<leader>gg', '<cmd>FzfLua git_diff<cr>',  desc = 'FzfLua git diff',   remap = true }
+
     },
     opts = {
       winopts = {
